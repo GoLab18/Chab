c7ced8bda527d57645d5bfeda57bb04fba3a1954
GoLab18 <kuba1golab@gmail.com>
2024-08-12 22:16:15 +0200

    Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:chab/authentication/login_register.dart';
+import 'package:chab/firebase_options.dart';
 import 'package:chab/themes/light_mode.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
