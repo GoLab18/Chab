038a81bc6cc5a11c1abf900a3a80ab24ad809ac1
GoLab18 <kuba1golab@gmail.com>
2024-08-12 15:28:51 +0200

    Added a template for text fields

---
+++ lib/components/custom_textfield.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String hintText;
+  final TextEditingController inputController;
+  final bool isObscured;
+  
+  const CustomTextField({
+    super.key,
+    required this.hintText,
+    required this.inputController,
+    this.isObscured = false
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: isObscured,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Theme.of(context).colorScheme.secondary,
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.tertiary
+          )
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Theme.of(context).colorScheme.primary
+          )
+        ),
+        hintText: hintText,
+        hintStyle: TextStyle(
+          color: Theme.of(context).hintColor
+        )
+      )
+    );
+  }
+}
