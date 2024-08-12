598bafa0e4a370f34fa47f2fc53a60d86bf8f3fd
GoLab18 <kuba1golab@gmail.com>
2024-08-12 15:30:09 +0200

    Added dark mode ThemeData

---
+++ lib/themes/dark_mode.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+  colorScheme: const ColorScheme.light(
+    surface: Color.fromARGB(255, 182, 200, 226),
+    primary: Color.fromARGB(255, 226, 233, 243),
+    secondary: Color.fromARGB(255, 111, 147, 195),
+    tertiary: Color.fromARGB(255, 30, 48, 72),
+    inversePrimary: Color.fromARGB(255, 24, 38, 58)
+  ),
+  hintColor: const Color.fromARGB(255, 115, 127, 144),
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    foregroundColor: Color.fromARGB(255, 226, 233, 243),
+    backgroundColor: Color.fromARGB(255, 66, 106, 158)
+  ),
+  drawerTheme: const DrawerThemeData(
+    backgroundColor: Color.fromARGB(255, 140, 169, 207)
+  )
+);
