4fb6aa69a19c26d785c9276525289f0c2c843139
GoLab18 <kuba1golab@gmail.com>
2024-08-12 19:08:13 +0200

    Wrapped in a SingleChildScrollView

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -20,93 +20,95 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-        
-            // Main logo
-            Icon(
-              Icons.message_sharp,
-              size: 60,
-              color: Theme.of(context).colorScheme.primary
-            ),
-        
-            const SizedBox(height: 40),
-            
-            // Message
-            Text(
-              "Welcome back!",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.primary,
-                fontSize: 16
-              )
-            ),
-        
-            const SizedBox(height: 20),
-        
-            // Login email
-            CustomTextField(
-              hintText: "Email..",
-              inputController: _emailController
-            ),
-        
-            const SizedBox(height: 20),
-        
-            // Login password
-            CustomTextField(
-              hintText: "Password..",
-              inputController: _pwdController,
-              isObscured: true
-            ),
-        
-            const SizedBox(height: 10),
-        
-            // Change password if forgotten
-            Text(
-              "Forgot password?",
-              textAlign: TextAlign.end,
-              style: TextStyle(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+          
+              // Main logo
+              Icon(
+                Icons.message_sharp,
+                size: 60,
                 color: Theme.of(context).colorScheme.primary
-              )
-            ),
-        
-            const SizedBox(height: 10),
-        
-            // login button
-            ButtonTemplate(
-              buttonText: "Login",
-              onButtonPressed: login
-            ),
-        
-            const SizedBox(height: 20),
-        
-            // Register instead
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Don't have an account? ",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.tertiary
-                  )
-                ),
-                GestureDetector(
-                  onTap: toggleRegisterPage,
-                  child: Text(
-                    "Sign up",
+              ),
+          
+              const SizedBox(height: 40),
+              
+              // Message
+              Text(
+                "Welcome back!",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 16
+                )
+              ),
+          
+              const SizedBox(height: 20),
+          
+              // Login email
+              CustomTextField(
+                hintText: "Email..",
+                inputController: _emailController
+              ),
+          
+              const SizedBox(height: 20),
+          
+              // Login password
+              CustomTextField(
+                hintText: "Password..",
+                inputController: _pwdController,
+                isObscured: true
+              ),
+          
+              const SizedBox(height: 10),
+          
+              // Change password if forgotten
+              Text(
+                "Forgot password?",
+                textAlign: TextAlign.end,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary
+                )
+              ),
+          
+              const SizedBox(height: 10),
+          
+              // login button
+              ButtonTemplate(
+                buttonText: "Login",
+                onButtonPressed: login
+              ),
+          
+              const SizedBox(height: 20),
+          
+              // Register instead
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account? ",
                     style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontWeight: FontWeight.bold
+                      color: Theme.of(context).colorScheme.tertiary
                     )
                   ),
-                )
-              ]
-            )
-          ]
+                  GestureDetector(
+                    onTap: toggleRegisterPage,
+                    child: Text(
+                      "Sign up",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+                  )
+                ]
+              )
+            ]
+          ),
         ),
       )
     );
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -21,102 +21,104 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-        
-            // Main logo
-            Icon(
-              Icons.message_sharp,
-              size: 60,
-              color: Theme.of(context).colorScheme.primary
-            ),
-        
-            const SizedBox(height: 40),
-            
-            // Message
-            Text(
-              "Create your account",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.primary,
-                fontSize: 16
-              )
-            ),
-        
-            const SizedBox(height: 20),
-        
-            // Register email
-            CustomTextField(
-              hintText: "Email..",
-              inputController: _emailController
-            ),
-        
-            const SizedBox(height: 20),
-        
-            // Register password
-            CustomTextField(
-              hintText: "Password..",
-              inputController: _pwdController,
-              isObscured: true
-            ),
-        
-            const SizedBox(height: 20),
-        
-            // Confirm password
-            CustomTextField(
-              hintText: "Confirm password..",
-              inputController: _confirmPwdController,
-              isObscured: true
-            ),
-        
-            const SizedBox(height: 10),
-        
-            // Change password if forgotten
-            Text(
-              "Forgot password?",
-              textAlign: TextAlign.end,
-              style: TextStyle(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+          
+              // Main logo
+              Icon(
+                Icons.message_sharp,
+                size: 60,
                 color: Theme.of(context).colorScheme.primary
-              )
-            ),
-        
-            const SizedBox(height: 10),
-        
-            // login button
-            ButtonTemplate(
-              buttonText: "Login",
-              onButtonPressed: register
-            ),
-        
-            const SizedBox(height: 20),
-        
-            // Register instead
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Already have an account? ",
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.tertiary
-                  )
-                ),
-                GestureDetector(
-                  onTap: toggleLoginPage,
-                  child: Text(
-                    "Login now",
+              ),
+          
+              const SizedBox(height: 40),
+              
+              // Message
+              Text(
+                "Create your account",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontSize: 16
+                )
+              ),
+          
+              const SizedBox(height: 20),
+          
+              // Register email
+              CustomTextField(
+                hintText: "Email..",
+                inputController: _emailController
+              ),
+          
+              const SizedBox(height: 20),
+          
+              // Register password
+              CustomTextField(
+                hintText: "Password..",
+                inputController: _pwdController,
+                isObscured: true
+              ),
+          
+              const SizedBox(height: 20),
+          
+              // Confirm password
+              CustomTextField(
+                hintText: "Confirm password..",
+                inputController: _confirmPwdController,
+                isObscured: true
+              ),
+          
+              const SizedBox(height: 10),
+          
+              // Change password if forgotten
+              Text(
+                "Forgot password?",
+                textAlign: TextAlign.end,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary
+                )
+              ),
+          
+              const SizedBox(height: 10),
+          
+              // login button
+              ButtonTemplate(
+                buttonText: "Login",
+                onButtonPressed: register
+              ),
+          
+              const SizedBox(height: 20),
+          
+              // Register instead
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Already have an account? ",
                     style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontWeight: FontWeight.bold
+                      color: Theme.of(context).colorScheme.tertiary
                     )
                   ),
-                )
-              ]
-            )
-          ]
+                  GestureDetector(
+                    onTap: toggleLoginPage,
+                    child: Text(
+                      "Login now",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+                  )
+                ]
+              )
+            ]
+          ),
         ),
       )
     );
