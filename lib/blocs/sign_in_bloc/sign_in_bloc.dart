40c8ea2f29b606a60a1b043945b674905e87f3cd
GoLab18 <kuba1golab@gmail.com>
2024-08-20 20:38:30 +0200

    Added bloc logic for sign in

---
+++ lib/blocs/sign_in_bloc/sign_in_bloc.dart
@@ -0,0 +1,31 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_repository/user_repository.dart';
+
+part 'sign_in_event.dart';
+part 'sign_in_state.dart';
+
+class SignInBloc extends Bloc<SignInEvent, SignInState> {
+  final UserRepository userRepository;
+  
+  SignInBloc({
+    required this.userRepository
+  }) : super(SignInInitial()) {
+    on<SignInRequested>((event, emit) async {
+      emit(SignInPending());
+
+      try {
+        await userRepository.signIn(event.email, event.password);
+
+        emit(SignInSuccess());
+      } catch (e) {
+        emit(const SignInFailure());
+      }
+    });
+
+    on<SignOutRequested>((event, emit) async {
+      await userRepository.logOut();
+    });
+  }
+}
