0253c70c513b2a56b1d6b279e69fd19759c891fe
GoLab18 <kuba1golab@gmail.com>
2024-08-21 00:54:03 +0200

    Added username and confirmation password validation

--- lib/util/auth_util/regex.dart
+++ lib/util/auth_util/regex.dart
@@ -1,5 +1,5 @@
-/// Returns an error message regarding email's requirements
-/// If all are met, falls back to a null value
+/// Returns an error message regarding email's requirements,
+/// if all are met, falls back to a null value
 String? validateEmail(String? email) {
   if (email!.isEmpty) {
     return "Email must be provided";
@@ -11,8 +11,8 @@ String? validateEmail(String? email) {
   return null;
 }
 
-/// Returns an error message regarding password's requirements
-/// If all are met, falls back to a null value
+/// Returns an error message regarding password's requirements,
+/// if all are met, falls back to a null value
 String? validatePassword(String? password) {
   if (password!.isEmpty) return 'Password must be provided';
   
@@ -33,5 +33,25 @@ String? validatePassword(String? password) {
   }
 
   // Password is valid
+  return null;
+}
+
+/// Returns an error message regarding confirmation password's requirements,
+/// if all are met, falls back to a null value
+String? validateConfirmPassword(String? password, String? confirmPassword) {
+  if (password!.isEmpty) return 'Password must be confirmed';
+
+  if (password != confirmPassword) return 'Passwords don\'t match';
+
+  // Confirmation password is valid
+  return null;
+}
+
+/// Returns an error message regarding username's requirements,
+/// if all are met, falls back to a null value
+String? validateUsername(String? name) {
+  if (name!.isEmpty) return 'Username must be provided';
+
+  // Username valid
   return null;
 }
