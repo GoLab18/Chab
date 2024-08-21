b8930a1e9f7ebc0532f4e09c494dddbc14efd4d7
GoLab18 <kuba1golab@gmail.com>
2024-08-21 12:13:37 +0200

    Exchanged for a light green palette

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
   colorScheme: const ColorScheme.light(
-    surface: Color.fromARGB(255, 182, 200, 226),
-    primary: Color.fromARGB(255, 226, 233, 243),
-    secondary: Color.fromARGB(255, 111, 147, 195),
-    tertiary: Color.fromARGB(255, 30, 48, 72),
-    inversePrimary: Color.fromARGB(255, 24, 38, 58)
+    surface: Color.fromARGB(255, 239, 247, 242),
+    primary: Color.fromARGB(255, 34, 85, 52),
+    secondary: Color.fromARGB(255, 133, 193, 153),
+    tertiary: Color.fromARGB(255, 210, 234, 221),
+    inversePrimary: Color.fromARGB(255, 24, 58, 38)
   ),
   hintColor: const Color.fromARGB(255, 147, 164, 186),
   floatingActionButtonTheme: const FloatingActionButtonThemeData(
-    foregroundColor: Color.fromARGB(255, 226, 233, 243),
-    backgroundColor: Color.fromARGB(255, 66, 106, 158)
+    foregroundColor: Color.fromARGB(255, 239, 247, 242),
+    backgroundColor: Color.fromARGB(255, 34, 85, 52)
   ),
   drawerTheme: const DrawerThemeData(
-    backgroundColor: Color.fromARGB(255, 140, 169, 207)
+    backgroundColor: Color.fromARGB(255, 133, 193, 153)
   )
 );
