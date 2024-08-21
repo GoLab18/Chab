e2b6f74f320b66d38d97e526f839a5fd9911ca33
GoLab18 <kuba1golab@gmail.com>
2024-08-21 18:04:20 +0200

    Added an app bar with searching utility

---
+++ lib/components/custom_app_bar.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  State<CustomAppBar> createState() => _CustomAppBarState();
+  
+  // PreferredSizeWidget interface implementation
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _CustomAppBarState extends State<CustomAppBar> {
+  bool isInSearchMode = false;
+
+  // Replaces the app bar components with the search bar
+  void toggleSearchMode() {
+    setState(() {
+      isInSearchMode = !isInSearchMode;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.secondary,
+      elevation: 10,
+      automaticallyImplyLeading: !isInSearchMode,
+      title: isInSearchMode
+        ? SearchBar(
+          elevation: const WidgetStatePropertyAll(0),
+          shape: const WidgetStatePropertyAll(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(0)
+              )
+            )
+          ),
+          hintText: "Search..",
+          hintStyle: WidgetStatePropertyAll(
+            TextStyle(
+              color: Theme.of(context).hintColor
+            ),
+          ),
+          backgroundColor: const WidgetStatePropertyAll(
+            Colors.transparent
+          ),
+          autoFocus: true
+        )
+        : Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Text(
+            "Chab",
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.inversePrimary,
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            )
+          ),
+        ),
+      centerTitle: false,
+      titleSpacing: 0,
+      leadingWidth: 30,
+      leading: isInSearchMode
+        ? IconButton(
+          onPressed: toggleSearchMode,
+          icon: Icon(
+            Icons.arrow_back,
+            color: Theme.of(context).colorScheme.inversePrimary
+          )
+        )
+        : null,
+      actions: <IconButton>[
+        IconButton(
+          onPressed: isInSearchMode
+            ? () {
+              // TODO Searching
+              toggleSearchMode();
+            }
+            : toggleSearchMode,
+          icon: Icon(
+            Icons.search_outlined,
+            color: Theme.of(context).colorScheme.inversePrimary
+          )
+        )
+      ]
+    );
+  }
+}
