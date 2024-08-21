46dd5f474bdfc0cc6ac0c686b465e2c6f0d47141
GoLab18 <kuba1golab@gmail.com>
2024-08-21 12:13:26 +0200

    Exchanged for a dark green palette

--- lib/themes/dark_mode.dart
+++ lib/themes/dark_mode.dart
@@ -3,17 +3,17 @@ import 'package:flutter/material.dart';
 ThemeData darkMode = ThemeData(
   brightness: Brightness.dark,
   colorScheme: const ColorScheme.dark(
-    surface: Color.fromARGB(255, 18, 29, 43),
-    primary: Color.fromARGB(255, 24, 38, 58),
-    secondary: Color.fromARGB(255, 82, 126, 183),
-    tertiary: Color.fromARGB(255, 154, 179, 213),
-    inversePrimary: Color.fromARGB(255, 226, 233, 243)
+    surface: Color.fromARGB(255, 18, 29, 23),
+    primary: Color.fromARGB(255, 38, 94, 58),
+    secondary: Color.fromARGB(255, 72, 115, 85),
+    tertiary: Color.fromARGB(255, 108, 158, 124),
+    inversePrimary: Color.fromARGB(255, 210, 234, 221)
   ),
   floatingActionButtonTheme: const FloatingActionButtonThemeData(
-    foregroundColor: Color.fromARGB(255, 226, 233, 243),
-    backgroundColor: Color.fromARGB(255, 48, 77, 115)
+    foregroundColor: Color.fromARGB(255, 210, 234, 221),
+    backgroundColor: Color.fromARGB(255, 38, 94, 58)
   ),
   drawerTheme: const DrawerThemeData(
-    backgroundColor: Color.fromARGB(255, 60, 96, 144)
+    backgroundColor: Color.fromARGB(255, 38, 94, 58)
   )
 );
