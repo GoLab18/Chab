0de8892c0212917a0ba8bdde9c33dde8becf2e74
GoLab18 <kuba1golab@gmail.com>
2024-08-22 22:19:42 +0200

    Deleted an export

--- packages/user_repository/lib/user_repository.dart
+++ packages/user_repository/lib/user_repository.dart
@@ -2,6 +2,5 @@ library user_repository;
 
 export 'src/models/models.dart';
 export 'src/entities/entities.dart';
-export 'src/user_repo.dart';
 export 'src/firebase_user_repository.dart';
 export 'src/util/result.dart';
