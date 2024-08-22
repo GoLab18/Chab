20e2c78d111a1663df2ea21e1a222a0c2e1d81fd
GoLab18 <kuba1golab@gmail.com>
2024-08-22 22:19:21 +0200

    Added a class that handles message related data
    It stores Message data fields and contains serialization methods

---
+++ packages/room_repository/lib/src/models/message.dart
@@ -0,0 +1,91 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:equatable/equatable.dart';
+
+import '../util/typedefs.dart';
+
+class Message extends Equatable {
+  /// Message content.
+  final String content;
+
+  /// Flag for prompting other users if the message was edited.
+  final bool edited;
+
+  /// Message picture.
+  final String picture;
+
+  /// A representation of firebase array.
+  /// Holds a [List] of user ids that have seen the message.
+  late final List<String> seenBy;
+
+  /// Id of the user that sent the message to the chat room.
+  final String senderId;
+
+  /// Message creation date.
+  /// Isn't overriden on edit.
+  late final Timestamp timestamp;
+
+  Message({
+    required this.content,
+    this.edited = false,
+    required this.picture,
+    List<String>? seenBy,
+    required this.senderId,
+    Timestamp? timestamp
+  }) {
+    this.seenBy = seenBy ?? List.empty();
+    this.timestamp = timestamp ?? Timestamp.now();
+  }
+  
+  @override
+  List<Object?> get props => [content, edited, seenBy, senderId, timestamp];
+
+  static Message empty = Message(
+    content: "",
+    picture: "",
+    senderId: ""
+  );
+
+  /// Getter for checking if the message is empty.
+  bool get isEmpty => this == Message.empty;
+
+  /// Returns a copy of the message with the given values.
+  Message copyWith({
+    String? content,
+    bool? edited,
+    String? picture,
+    List<String>? seenBy,
+    String? senderId
+  }) {
+    return Message(
+      content: content ?? this.content,
+      edited: edited ?? this.edited,
+      picture: picture ?? this.picture,
+      seenBy: seenBy ?? this.seenBy,
+      senderId: senderId ?? this.senderId
+    );
+  }
+  
+  /// Data serialization for database storage.
+  JsonMap toDocument() {
+    return {
+      "content": content,
+      "edited": edited,
+      "picture": picture,
+      "seenBy": seenBy,
+      "senderId": senderId,
+      "timestamp": timestamp
+    };
+  }
+  
+  /// Database data deserialization.
+  static Message fromDocument(JsonMap doc) {
+    return Message(
+      content: doc["content"] as String,
+      edited: doc["edited"] as bool,
+      picture: doc["picture"] as String,
+      seenBy: doc["seenBy"] as List<String>,
+      senderId: doc["senderId"] as String,
+      timestamp: doc["timestamp"] as Timestamp
+    );
+  }
+}
