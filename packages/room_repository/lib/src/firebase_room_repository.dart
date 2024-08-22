0bb6111f95501960c85e2df5ba3d43a924845641
GoLab18 <kuba1golab@gmail.com>
2024-08-22 22:15:53 +0200

    Added missing doc comments

--- packages/room_repository/lib/src/firebase_room_repository.dart
+++ packages/room_repository/lib/src/firebase_room_repository.dart
@@ -36,26 +36,32 @@ class FirebaseRoomRepository {
     );
   }
 
+  /// Adds a new room to the firebase rooms collection.
   Future<void> addRoom(Room room) async {
     await roomsCollection.add(room.toDocument());
   }
 
+  /// Adds a new message to the firebase messages subcollection.
   Future<void> addMessage(String roomId, Message message) async {
     await roomsCollection.doc(roomId).collection("messages").add(message.toDocument());
   }
 
+  /// Updates a room in the firestore.
   Future<void> updateRoomData(String roomId, Room updatedRoom) async {
     await roomsCollection.doc(roomId).update(updatedRoom.toDocument());
   }
 
+  /// Updates a message in the firestore
   Future<void> updateMessage(String roomId, String messageId, Message updatedMsg) async {
     await roomsCollection.doc(roomId).collection("messages").doc(messageId).update(updatedMsg.toDocument());
   }
 
+  /// Deletes a room from to the firebase rooms collection.
   Future<void> deleteRoom(String roomId) async {
     await roomsCollection.doc(roomId).delete();
   }
 
+  /// Deletes a message from the firebase messages subcollection.
   Future<void> deleteMessage(String roomId, String messageId) async {
     await roomsCollection.doc(roomId).collection("messages").doc(messageId).delete();
   }
