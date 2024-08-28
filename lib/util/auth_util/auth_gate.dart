a2073a6189275c94f5a890de0ff3e9ae908f70c5
GoLab18 <kuba1golab@gmail.com>
2024-08-28 14:22:05 +0200

    Added a RepositoryProvider for chat rooms

--- lib/util/auth_util/auth_gate.dart
+++ lib/util/auth_util/auth_gate.dart
@@ -1,24 +1,32 @@
-import 'package:chab/util/auth_util/login_register.dart';
-import 'package:chab/blocs/auth_bloc/auth_bloc.dart';
-import 'package:chab/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:room_repository/room_repository.dart';
+
+import '../../blocs/auth_bloc/auth_bloc.dart';
+import '../../blocs/room_bloc/room_bloc.dart';
+import '../../pages/home_page.dart';
+import 'login_register.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocBuilder<AuthBloc, AuthState>(
+    return Scaffold(body: BlocBuilder<AuthBloc, AuthState>(
         builder: (BuildContext context, AuthState state) {
-          if (state.status == AuthStatus.authenticated) {
-            return const HomePage();
-          } else {
-            return const LoginRegister();
-          }
-        }
-      )
-    );
+      if (state.status == AuthStatus.authenticated) {
+        return RepositoryProvider(
+          create: (context) => FirebaseRoomRepository(),
+          child: BlocProvider(
+            create: (context) => RoomBloc(
+              roomRepository: context.read<FirebaseRoomRepository>()
+            ),
+            child: const HomePage()
+          )
+        );
+      } else {
+        return const LoginRegister();
+      }
+    }));
   }
 }
