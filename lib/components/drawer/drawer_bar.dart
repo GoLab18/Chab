76c28d383a6dc8f4b045136afbd7aa61a0b6ebb1
GoLab18 <kuba1golab@gmail.com>
2024-08-31 22:51:42 +0200

    Added reactive loading

--- lib/components/drawer/drawer_bar.dart
+++ lib/components/drawer/drawer_bar.dart
@@ -5,22 +5,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../blocs/usr_bloc/usr_bloc.dart';
 
-class DrawerBar extends StatefulWidget {
+class DrawerBar extends StatelessWidget {
   const DrawerBar({super.key});
-
-  @override
-  State<DrawerBar> createState() => _DrawerBarState();
-}
-
-class _DrawerBarState extends State<DrawerBar> {
-  // TODO
-  // final String username = context.read<UsrBloc>().;
-
-  void toggleThemes() {
-    context.read<ThemeBloc>().add(
-      ToggleThemes()
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,72 +16,99 @@ class _DrawerBarState extends State<DrawerBar> {
         right: 8,
         top: 8
       ),
-      child: ConstrainedBox(
-        constraints: const BoxConstraints.expand(height: 140),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  
-                  // Profile picture
-                  CircleAvatar(
-                    radius: 36,
-                    foregroundImage: NetworkImage(context.read<UsrBloc>().state.user!.picture),
-                    backgroundColor: Theme.of(context).colorScheme.tertiary,
-                    child: Icon(
-                      Icons.person_outlined,
-                      color: Theme.of(context).colorScheme.inversePrimary
-                    )
-                  ),
-              
-                  // Dark mode toggle
-                  IconButton(
-                    onPressed: toggleThemes,
-                    icon: Icon(
-                      SharedPreferencesUtil.isDarkTheme
-                        ? Icons.dark_mode_outlined
-                        : Icons.light_mode_outlined,
-                      color: Theme.of(context).colorScheme.inversePrimary
-                    )
-                  )
-                ]
-              ),
-            ),
+      child: BlocBuilder<UsrBloc, UsrState>(
+        builder: (context, state) {
+          if (state.status == UsrStatus.failure) {
+            return Text(
+              "Loading error",
+              style: TextStyle(
+                fontSize: 20,
+                color: Theme.of(context).colorScheme.inversePrimary
+              )
+            );
+          }
 
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-              
-                  // Username
-                  Text(
-                    context.read<UsrBloc>().state.user!.name,
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.inversePrimary
-                    )
-                  ),
+          return ConstrainedBox(
+            constraints: const BoxConstraints.expand(height: 140),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      
+                      // Profile picture
+                      CircleAvatar(
+                        radius: 36,
+                        foregroundImage: (state.status == UsrStatus.success)
+                          ? NetworkImage(state.user!.picture)
+                          : null,
+                        backgroundColor: Theme.of(context).colorScheme.tertiary,
+                        child: Icon(
+                          Icons.person_outlined,
+                          color: Theme.of(context).colorScheme.inversePrimary
+                        )
+                      ),
                   
-                  // Username
-                  Text(
-                    // TODO change it for: online, offline, unseen accordingly etc.
-                    // (either based on the database field or idk, shared_references?)
-                    // Status choice menu (?)
-                    "online",
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.tertiary
-                    )
+                      // Dark mode toggle
+                      IconButton(
+                        onPressed: () => context.read<ThemeBloc>().add(ToggleThemes()),
+                        icon: Icon(
+                          SharedPreferencesUtil.isDarkTheme
+                            ? Icons.dark_mode_outlined
+                            : Icons.light_mode_outlined,
+                          color: Theme.of(context).colorScheme.inversePrimary
+                        )
+                      )
+                    ]
+                  ),
+                ),
+    
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                  
+                      // Username
+                      (state.status == UsrStatus.success)
+                        ? Text(
+                          state.user!.name,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).colorScheme.inversePrimary
+                          )
+                        )
+                        : SizedBox(
+                          width: 16,
+                          height: 16,
+                          child: CircularProgressIndicator(
+                            strokeWidth: 2,
+                            color: Theme.of(context).colorScheme.inversePrimary,
+                          ),
+                        ),
+                      
+                      // Status
+                      Text(
+                        // TODO change it for: online, offline, unseen accordingly etc.
+                        // (either based on the database field or idk, shared_references?)
+                        // Status choice menu (?)
+                        "online",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.tertiary
+                        )
+                      )
+                    ]
                   )
-                ]
-              ),
+                )
+              ]
             )
-          ],
-        ),
+          );
+        }
       ),
     );
   }
