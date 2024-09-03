03c0228f45a01b89de796a0fadc6eeb172b16f3c
GoLab18 <kuba1golab@gmail.com>
2024-09-04 00:47:11 +0200

    List<Message> is now Stream<List<Message>>

--- packages/room_repository/lib/src/util/chat_room_tuple.dart
+++ packages/room_repository/lib/src/util/chat_room_tuple.dart
@@ -3,10 +3,10 @@ import '../models/models.dart';
 /// Tuple mixing single [Room] with it's [Message]s
 class ChatRoomTuple {
   final Room room;
-  final List<Message> messages;
+  final Stream<List<Message>> messagesStream;
 
   ChatRoomTuple({
     required this.room,
-    required this.messages
+    required this.messagesStream
   });
 }
