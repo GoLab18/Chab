bece63eb538470f2d1149a9d61eb6a1c8de6937a
GoLab18 <kuba1golab@gmail.com>
2024-09-03 13:01:29 +0200

    Added chat room page

---
+++ lib/pages/chat_room_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChatRoomPage extends StatefulWidget {
+  const ChatRoomPage({super.key});
+
+  @override
+  State<ChatRoomPage> createState() => _ChatRoomPageState();
+}
+
+class _ChatRoomPageState extends State<ChatRoomPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar()
+    );
+  }
+}
