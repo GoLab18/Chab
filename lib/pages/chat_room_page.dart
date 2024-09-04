951fbbd7ad4f3abb1e44e621b17e7281323acb66
GoLab18 <kuba1golab@gmail.com>
2024-09-04 18:54:46 +0200

    Added messages list and adding a new message
    Added a ListView that builds depending on a StreamBuilder with messages. Added a bottom bar for sending messages with button enabling only on a new message field not being empty.

--- lib/pages/chat_room_page.dart
+++ lib/pages/chat_room_page.dart
@@ -1,6 +1,14 @@
+import 'package:chab/components/message_bubble.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:room_repository/room_repository.dart';
 
+import '../blocs/message_bloc/message_bloc.dart';
+import '../blocs/room_bloc/room_bloc.dart';
+import '../blocs/usr_bloc/usr_bloc.dart';
 import '../components/app_bars/chat_room_app_bar.dart';
+import '../components/is_empty_message_widget.dart';
 
 class ChatRoomPage extends StatefulWidget {
   const ChatRoomPage({super.key});
@@ -10,10 +18,180 @@ class ChatRoomPage extends StatefulWidget {
 }
 
 class _ChatRoomPageState extends State<ChatRoomPage> {
+  late ScrollController scrollController;
+  late TextEditingController newMessageController;
+  bool isSendButtonEnabled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    
+    scrollController = ScrollController()..addListener(scrollLoading);
+    newMessageController = TextEditingController()..addListener(buttonEnabling);
+  }
+
+  // TODO loading on scroll
+  void scrollLoading() {
+    if (scrollController.position.pixels == scrollController.position.maxScrollExtent) null;
+  }
+
+  void buttonEnabling() {
+    if (isSendButtonEnabled != newMessageController.text.isNotEmpty) {
+      setState(() {
+        isSendButtonEnabled = newMessageController.text.isNotEmpty;
+      });
+    }
+  }
+
+  void pickPhoto() {
+
+  }
+
+  @override
+  void dispose() {
+    scrollController.removeListener(scrollLoading);
+    scrollController.dispose();
+    newMessageController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: ChatRoomAppBar()
+      appBar: const ChatRoomAppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Messages dynamic loading
+          Expanded(
+            child: Center(
+              child: BlocBuilder<RoomBloc, RoomState>(
+                builder: (context, state) {
+                  if (state.status == ChatRoomStatus.failure) {
+                    return Text(
+                      "Loading error",
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Theme.of(context).colorScheme.inversePrimary
+                      )
+                    );
+                  } else if (state.status == ChatRoomStatus.loading) {
+                    return const CircularProgressIndicator();
+                  } else if (state.status == ChatRoomStatus.success) {
+                    return StreamBuilder(
+                      stream: state.roomTuple!.messagesStream,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.waiting) return const CircularProgressIndicator();
+              
+                        if (snapshot.hasError) {
+                          return Text(
+                            "Loading error",
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Theme.of(context).colorScheme.inversePrimary
+                            )
+                          );
+                        }
+                        
+                        if (!snapshot.hasData || snapshot.data!.isEmpty) return const IsEmptyMessageWidget();
+                        
+                        final messages = snapshot.data;
+              
+                        return Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: ListView.separated(
+                            reverse: true,
+                            controller: scrollController,
+                            itemCount: messages!.length,
+                            separatorBuilder: (context, index) => const SizedBox(height: 8),
+                            itemBuilder: (BuildContext context, int index) {
+                              var message = messages[index];
+                              bool isCurrentUserMessage = message.senderId == context.read<UsrBloc>().state.user!.id;
+              
+                              return Column(
+                                crossAxisAlignment: (isCurrentUserMessage)
+                                  ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                                children: [
+                                  MessageBubble(
+                                    message: message,
+                                    isCurrentUserMessage: isCurrentUserMessage
+                                  )
+                                ]
+                              );
+                            }
+                          )
+                        );
+                      }
+                    );
+                  }
+              
+                  throw Exception("Non-existent room_bloc state");
+                }
+              )
+            )
+          ),
+
+          // New message field
+          Container(
+            height: 60,
+            color: Theme.of(context).colorScheme.primary,
+            child: Center(
+              child: TextFormField(
+                controller: newMessageController,
+                onTapOutside: (event) => FocusScope.of(context).unfocus(),
+                maxLines: null,
+                inputFormatters: [
+                  LengthLimitingTextInputFormatter(4000)
+                ],
+                cursorColor: Theme.of(context).colorScheme.inversePrimary,
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.done,
+                textAlignVertical: TextAlignVertical.center,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  prefixIcon: IconButton(
+                    onPressed: pickPhoto,
+                    icon: Icon(
+                      Icons.photo_outlined,
+                      color: Theme.of(context).colorScheme.tertiary
+                    )
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: isSendButtonEnabled
+                      ? () {
+                        context.read<MessageBloc>().add(AddMessage(
+                        roomId: context.read<RoomBloc>().state.roomTuple!.room.id,
+                        message: Message(
+                          id: "", // Will be auto generated in the backend.
+                          content: newMessageController.text,
+                          senderId: context.read<UsrBloc>().state.user!.id
+                        )
+                      ));
+
+                        newMessageController.clear();
+                        FocusScope.of(context).unfocus();
+                      }
+                      : null,
+                    icon: Icon(
+                      Icons.send_outlined,
+                      color: isSendButtonEnabled
+                        ? Theme.of(context).colorScheme.inversePrimary
+                        : Theme.of(context).colorScheme.tertiary
+                    )
+                  ),
+                  filled: true,
+                  fillColor: Colors.transparent,
+                  hintText: "Message..",
+                  hintStyle: TextStyle(
+                    color: Theme.of(context).hintColor
+                  )
+                )
+              )
+            )
+          )
+        ]
+      )
     );
   }
 }
