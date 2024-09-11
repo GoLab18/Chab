d7d628bd1fffa70a2869bd8d52e54d422bc7c304
GoLab18 <kuba1golab@gmail.com>
2024-09-12 00:33:19 +0200

    Added loading also based on UsrBloc status

--- lib/pages/chat_rooms_list_page.dart
+++ lib/pages/chat_rooms_list_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:user_repository/user_repository.dart';
 
+import '../blocs/bloc/room_members_bloc.dart';
 import '../blocs/rooms_bloc/rooms_bloc.dart';
-import '../components/is_empty_message_widget.dart';
+import '../blocs/usr_bloc/usr_bloc.dart';
 import '../components/chat_room_tile.dart';
 
 class ChatRoomsListPage extends StatelessWidget {
@@ -13,15 +15,53 @@ class ChatRoomsListPage extends StatelessWidget {
     ScrollController scrollController = ScrollController();
 
     return Center(
-      child: BlocBuilder<RoomsBloc, RoomsState>(
-        builder: (context, state) {
-          if (state.status == RoomsStatus.success) {
-            return StreamBuilder(
-              stream: state.roomsList,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) return const CircularProgressIndicator();
-
-                if (snapshot.hasError) {
+      child: BlocBuilder<UsrBloc, UsrState>(
+        builder: (context, usrState) {
+          return BlocBuilder<RoomsBloc, RoomsState>(
+              builder: (context, roomsState) {
+                if (roomsState.status == RoomsStatus.success && usrState.status == UsrStatus.success) {
+                  return StreamBuilder(
+                    stream: roomsState.roomsList,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) return const CircularProgressIndicator();
+      
+                      if (snapshot.hasError) {
+                        return Text(
+                          "Loading error",
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Theme.of(context).colorScheme.inversePrimary
+                          )
+                        );
+                      }
+      
+                      final rooms = snapshot.data;
+      
+                      return SizedBox(
+                        child: ListView.builder(
+                          controller: scrollController,
+                          itemCount: rooms!.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final room = rooms[index];
+      
+                            return BlocProvider(
+                            create: (context) => RoomMembersBloc(
+                              userRepository: context.read<FirebaseUserRepository>()
+                            )..add(
+                              room.isPrivate
+                                ? PrivateChatRoomMembersRequested(room.id)
+                                : GroupChatRoomMembersRequested(room.id)
+                            ),
+                            child: ChatRoomTile(rooms[index])
+                          );
+                          }
+                        )
+                      );
+                    }
+                  );
+                } else if (roomsState.status == RoomsStatus.loading || usrState.status == UsrStatus.loading) {
+                  return const CircularProgressIndicator();
+                } else if (roomsState.status == RoomsStatus.failure || usrState.status == UsrStatus.failure) {
                   return Text(
                     "Loading error",
                     style: TextStyle(
@@ -30,34 +70,11 @@ class ChatRoomsListPage extends StatelessWidget {
                     )
                   );
                 }
-
-                if (!snapshot.hasData || snapshot.data!.isEmpty) return const IsEmptyMessageWidget();
-
-                final rooms = snapshot.data;
-
-                return SizedBox(
-                  child: ListView.builder(
-                    controller: scrollController,
-                    itemCount: rooms!.length,
-                    itemBuilder: (BuildContext context, int index) => ChatRoomTile(rooms[index])
-                  )
-                );
+      
+                throw Exception("Non-existent rooms_bloc state");
               }
             );
-          } else if (state.status == RoomsStatus.loading) {
-            return const CircularProgressIndicator();
-          } else if (state.status == RoomsStatus.failure) {
-            return Text(
-              "Loading error",
-              style: TextStyle(
-                fontSize: 30,
-                color: Theme.of(context).colorScheme.inversePrimary
-              )
-            );
-          }
-
-          throw Exception("Non-existent rooms_bloc state");
-        }
+        },
       ),
     );
   }
