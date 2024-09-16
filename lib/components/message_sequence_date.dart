067d1ab66dbe46afb30e174592101fe52118fe04
GoLab18 <kuba1golab@gmail.com>
2024-09-16 23:16:06 +0200

    Added a divider date widget for message sequences

---
+++ lib/components/message_sequence_date.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class MessageSequenceDate extends StatelessWidget {
+  final String dateString;
+
+  const MessageSequenceDate(this.dateString, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 4,
+          vertical: 14
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Divider(
+                color: Theme.of(context).dividerColor,
+                thickness: 0.6
+              )
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 6),
+              child: Container(
+                color: Colors.transparent,
+                child: Text(
+                  dateString,
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Theme.of(context).colorScheme.inversePrimary
+                  )
+                )
+              )
+            ),
+            Expanded(
+              child: Divider(
+                color: Theme.of(context).dividerColor,
+                thickness: 0.6
+              )
+            )
+          ]
+        )
+      )
+    );
+  }
+}
