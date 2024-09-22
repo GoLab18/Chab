54eb590a6e04b3d8aa01e9300d7e3fff2fc12ccb
GoLab18 <kuba1golab@gmail.com>
2024-09-22 21:23:11 +0200

    Changed util-related exports

--- packages/room_repository/lib/room_repository.dart
+++ packages/room_repository/lib/room_repository.dart
@@ -2,4 +2,4 @@ library room_repository;
 
 export 'src/models/models.dart';
 export 'src/firebase_room_repository.dart';
-export 'src/util/chat_room_tuple.dart';
+export 'src/util/util.dart';
--- packages/user_repository/lib/user_repository.dart
+++ packages/user_repository/lib/user_repository.dart
@@ -2,4 +2,4 @@ library user_repository;
 
 export 'src/models/models.dart';
 export 'src/firebase_user_repository.dart';
-export 'src/util/result.dart';
+export 'src/util/util.dart';
