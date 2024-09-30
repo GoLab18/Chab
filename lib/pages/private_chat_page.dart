63f9862ed29d29bec566b6039646830570baea44
GoLab18 <kuba1golab@gmail.com>
2024-09-30 22:55:51 +0200

    Added a page for private chat info

---
+++ lib/pages/private_chat_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:user_repository/user_repository.dart';
+
+class PrivateChatPage extends StatelessWidget {
+  final Usr initialData;
+  
+  const PrivateChatPage(this.initialData, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar()
+    );
+  }
+}
