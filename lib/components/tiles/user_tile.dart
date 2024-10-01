c77ae271f5c4442cb6190f66da886328343458bd
GoLab18 <kuba1golab@gmail.com>
2024-10-01 21:08:47 +0200

    Added horizontal padding

--- lib/components/tiles/user_tile.dart
+++ lib/components/tiles/user_tile.dart
@@ -19,75 +19,78 @@ class UserTile extends StatelessWidget {
         child: InkWell(
           onTap: () => navigateToUserPage(context),
           splashColor: Theme.of(context).colorScheme.tertiary.withOpacity(0.5),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              CircleAvatar(
-                radius: 24,
-                foregroundImage: user.picture.isNotEmpty
-                  ? NetworkImage(user.picture)
-                  : null,
-                backgroundColor: Theme.of(context).colorScheme.tertiary,
-                child: Icon(
-                  Icons.person_outlined,
-                  color: Theme.of(context).colorScheme.inversePrimary
-                )
-              ),
-                  
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    left: 10,
-                    top: 2,
-                    bottom: 2
-                  ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                CircleAvatar(
+                  radius: 24,
+                  foregroundImage: user.picture.isNotEmpty
+                    ? NetworkImage(user.picture)
+                    : null,
+                  backgroundColor: Theme.of(context).colorScheme.tertiary,
+                  child: Icon(
+                    Icons.person_outlined,
+                    color: Theme.of(context).colorScheme.inversePrimary
+                  )
+                ),
+                    
+                Expanded(
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 14),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            // Username
-                            Flexible(
-                              child: Padding(
-                                padding: const EdgeInsets.only(right: 14),
-                                child: Text(
-                                  user.name,
-                                  style: TextStyle(
-                                    color: Theme.of(context).colorScheme.inversePrimary
+                    padding: const EdgeInsets.only(
+                      left: 10,
+                      top: 2,
+                      bottom: 2
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 14),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              // Username
+                              Flexible(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(right: 14),
+                                  child: Text(
+                                    user.name,
+                                    style: TextStyle(
+                                      color: Theme.of(context).colorScheme.inversePrimary
+                                    )
                                   )
                                 )
+                              ),
+            
+                              // TODO Nickname
+                              // if (nickname is present)
+                              Text(
+                                "Lil Chigga",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Theme.of(context).colorScheme.tertiary
+                                )
                               )
-                            ),
-
-                            // TODO Nickname
-                            // if (nickname is present)
-                            Text(
-                              "Lil Chigga",
-                              style: TextStyle(
-                                fontSize: 12,
-                                color: Theme.of(context).colorScheme.tertiary
-                              )
+                            ]
+                          ),
+            
+                          // TODO Chat room role
+                          Text(
+                            "Member", // if Admin then text green
+                            style: TextStyle(
+                              color: Theme.of(context).colorScheme.tertiary
                             )
-                          ]
-                        ),
-
-                        // TODO Chat room role
-                        Text(
-                          "Member", // if Admin then text green
-                          style: TextStyle(
-                            color: Theme.of(context).colorScheme.tertiary
                           )
-                        )
-                      ]
+                        ]
+                      )
                     )
                   )
                 )
-              )
-            ]
+              ]
+            ),
           )
         )
       )
