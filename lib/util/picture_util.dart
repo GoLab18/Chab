9d3749294b5ef604270b60a7b19b795d4f23e19f
GoLab18 <kuba1golab@gmail.com>
2024-10-02 22:00:49 +0200

    Added PictureUtil class with uploadAndCropPicture method

---
+++ lib/util/picture_util.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+
+/// Utility class for image related operations.
+class PictureUtil {
+
+  /// Takes care of fetching a picture from phone's gallery.
+  /// 
+  /// Requires the [context] of the parent widget, 
+  /// [mounted] getter that is supposed to be set null on stateless widget calling this method and
+  /// [onImagePicked] function that has an image path [String] for use.
+  static Future<void> uploadAndCropPicture(BuildContext context, bool? mounted, void Function(String) onImagePicked) async {
+    Color primaryColor = Theme.of(context).colorScheme.primary;
+    Color toolbarWidgetColor = Theme.of(context).colorScheme.inversePrimary;
+    
+    ImagePicker imagePicker = ImagePicker();
+
+    XFile? image = await imagePicker.pickImage(
+      source: ImageSource.gallery,
+      maxHeight: 320,
+      maxWidth: 320,
+      imageQuality: 60
+    );
+
+    CroppedFile? croppedImage;
+    
+    if (image != null) {
+      croppedImage = await ImageCropper().cropImage(
+        sourcePath: image.path,
+        aspectRatio: const CropAspectRatio(
+          ratioX: 1,
+          ratioY: 1
+        ),
+        uiSettings: [
+          AndroidUiSettings(
+            toolbarTitle: "Crop Image",
+            toolbarColor: primaryColor,
+            activeControlsWidgetColor: primaryColor,
+            toolbarWidgetColor: toolbarWidgetColor,
+            aspectRatioPresets: [
+              CropAspectRatioPreset.square
+            ]
+          ),
+          IOSUiSettings(
+            title: "Crop Image",
+            aspectRatioPresets: [
+              CropAspectRatioPreset.square
+            ]
+          )
+        ]
+      );
+    }
+
+    if ((mounted == null || mounted) && croppedImage != null) {
+      onImagePicked(croppedImage.path);
+    }
+  }
+}
