dd4910b8df27862fe62b6e8dc59bd40ebe604d3c
GoLab18 <kuba1golab@gmail.com>
2024-10-30 21:37:26 +0100

    Added InkWell on tap effect

--- lib/components/tiles/invite_tile.dart
+++ lib/components/tiles/invite_tile.dart
@@ -45,62 +45,52 @@ class _InviteTileState extends State<InviteTile> {
   Widget build(BuildContext context) {
     final InviteStatus status = widget.invite.status;
 
-    return Padding(
-      padding: const EdgeInsets.all(4),
-      child: SizedBox(
-        height: 48,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // Sender profile picture
-            CircleAvatar(
-              radius: 24,
-              foregroundImage: widget.user.picture.isNotEmpty
-                ? NetworkImage(widget.user.picture)
-                : null,
-              backgroundColor: Theme.of(context).colorScheme.tertiary,
-              child: Icon(
-                Icons.person_outlined,
-                color: Theme.of(context).colorScheme.inversePrimary
-              )
-            ),
-
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 12,
-                  right: 8
+    return Material(
+      child: InkWell(
+        onTap: () {},
+        child: Padding(
+          padding: const EdgeInsets.all(4),
+          child: SizedBox(
+            height: 48,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Sender profile picture
+                CircleAvatar(
+                  radius: 24,
+                  foregroundImage: widget.user.picture.isNotEmpty
+                    ? NetworkImage(widget.user.picture)
+                    : null,
+                  backgroundColor: Theme.of(context).colorScheme.tertiary,
+                  child: Icon(
+                    Icons.person_outlined,
+                    color: Theme.of(context).colorScheme.inversePrimary
+                  )
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // Username of the sender/receiver
-                    Text(
-                      widget.user.name,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                      style: TextStyle(
-                        color: Theme.of(context).colorScheme.inversePrimary
-                      )
+        
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 12,
+                      right: 8
                     ),
-
-                    widget.tabIndex == 0
-                    // Invite sending date
-                      ? Text(
-                        DateUtil.getLongDateFormatFromNow(widget.invite.timestamp.toDate()),
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Theme.of(context).colorScheme.tertiary
-                        )
-                      )
-                      : Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          // Invite sending date
-                          Text(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Username of the sender/receiver
+                        Text(
+                          widget.user.name,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.inversePrimary
+                          )
+                        ),
+        
+                        widget.tabIndex == 0
+                        // Invite sending date
+                          ? Text(
                             DateUtil.getLongDateFormatFromNow(widget.invite.timestamp.toDate()),
                             overflow: TextOverflow.ellipsis,
                             maxLines: 1,
@@ -108,89 +98,104 @@ class _InviteTileState extends State<InviteTile> {
                               fontSize: 12,
                               color: Theme.of(context).colorScheme.tertiary
                             )
-                          ),
-
-                          // Invite status
-                          Text(
-                            assertStatusString(status),
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 1,
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold,
-                              color: assertStatusColor(status)
-                            )
                           )
-                        ]
-                      )
-
-                  ]
+                          : Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              // Invite sending date
+                              Text(
+                                DateUtil.getLongDateFormatFromNow(widget.invite.timestamp.toDate()),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Theme.of(context).colorScheme.tertiary
+                                )
+                              ),
+        
+                              // Invite status
+                              Text(
+                                assertStatusString(status),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                  color: assertStatusColor(status)
+                                )
+                              )
+                            ]
+                          )
+        
+                      ]
+                    )
+                  )
+                ),
+        
+                // Accepts invite, setting it to a status "accepted"
+                if (widget.tabIndex == 0 && status == InviteStatus.pending) IconButton(
+                  onPressed: () {
+                    context.read<InvitesOperationsBloc>().add(UpdateInviteStatus(
+                      inviteId: widget.invite.id,
+                      newStatus: InviteStatus.accepted,
+                      fromUserId: widget.user.id
+                    ));
+        
+                    widget.onStatusChanged!(InviteStatus.accepted);
+                  },
+                  icon: Icon(
+                    Icons.check_circle_outlined,
+                    size: 26,
+                    color: Theme.of(context).colorScheme.inversePrimary
+                  )
+                ),
+        
+                // Declines invite, setting it to a status "declined"
+                if (widget.tabIndex == 0 && status == InviteStatus.pending) IconButton(
+                  onPressed: () {
+                    context.read<InvitesOperationsBloc>().add(UpdateInviteStatus(
+                      inviteId: widget.invite.id,
+                      newStatus: InviteStatus.declined
+                    ));
+        
+                    widget.onStatusChanged!(InviteStatus.declined);
+                  },
+                  icon: Icon(
+                    Icons.cancel_outlined,
+                    size: 26,
+                    color: Theme.of(context).colorScheme.secondary
+                  )
+                ),
+        
+                // Status prompt for when invite is already answered
+                if (widget.tabIndex == 0 && status != InviteStatus.pending) Padding(
+                  padding: const EdgeInsets.only(right: 16),
+                  child: Text(
+                    assertStatusString(status),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: assertStatusColor(status)
+                    )
+                  )
+                ),
+        
+                // Delete invite icon button
+                if (widget.tabIndex == 1) IconButton(
+                  onPressed: () => context.read<InvitesOperationsBloc>().add(DeleteInvite(widget.invite.id)),
+                  icon: Icon(
+                    Icons.cancel_outlined,
+                    size: 26,
+                    color: Theme.of(context).colorScheme.secondary
+                  )
                 )
-              )
-            ),
-
-            // Accepts invite, setting it to a status "accepted"
-            if (widget.tabIndex == 0 && status == InviteStatus.pending) IconButton(
-              onPressed: () {
-                context.read<InvitesOperationsBloc>().add(UpdateInviteStatus(
-                  inviteId: widget.invite.id,
-                  newStatus: InviteStatus.accepted,
-                  fromUserId: widget.user.id
-                ));
-
-                widget.onStatusChanged!(InviteStatus.accepted);
-              },
-              icon: Icon(
-                Icons.check_circle_outlined,
-                size: 26,
-                color: Theme.of(context).colorScheme.inversePrimary
-              )
-            ),
-
-            // Declines invite, setting it to a status "declined"
-            if (widget.tabIndex == 0 && status == InviteStatus.pending) IconButton(
-              onPressed: () {
-                context.read<InvitesOperationsBloc>().add(UpdateInviteStatus(
-                  inviteId: widget.invite.id,
-                  newStatus: InviteStatus.declined
-                ));
-
-                widget.onStatusChanged!(InviteStatus.declined);
-              },
-              icon: Icon(
-                Icons.cancel_outlined,
-                size: 26,
-                color: Theme.of(context).colorScheme.secondary
-              )
-            ),
-
-            // Status prompt for when invite is already answered
-            if (widget.tabIndex == 0 && status != InviteStatus.pending) Padding(
-              padding: const EdgeInsets.only(right: 16),
-              child: Text(
-                assertStatusString(status),
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                  color: assertStatusColor(status)
-                )
-              )
-            ),
-
-            // Delete invite icon button
-            if (widget.tabIndex == 1) IconButton(
-              onPressed: () => context.read<InvitesOperationsBloc>().add(DeleteInvite(widget.invite.id)),
-              icon: Icon(
-                Icons.cancel_outlined,
-                size: 26,
-                color: Theme.of(context).colorScheme.secondary
-              )
+              ]
             )
-          ]
-        )
-      )
+          )
+        ),
+      ),
     );
   }
 }
