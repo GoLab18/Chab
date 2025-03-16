d0f99da6e6c7e7ba32b4c6ccb60412d0a61e7342
GoLab18 <kuba1golab@gmail.com>
2025-03-16 18:17:29 +0100

    ES client and env setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:chab/firebase_options.dart';
 import 'package:chab/my_app.dart';
 import 'package:chab/util/orientation_util.dart';
 import 'package:chab/util/shared_preferences_util.dart';
+import 'package:es_client_repository/es_client_repository.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:room_repository/room_repository.dart';
 import 'package:user_repository/user_repository.dart';
 
@@ -20,14 +22,18 @@ void main() async {
 
   await lockPhoneOrientationVertical();
 
+  await dotenv.load();
+
+  final esClient = await EsConfig.setup(dotenv.env);
+
   runApp(
     MultiRepositoryProvider(
       providers: [
         RepositoryProvider(
-          create: (context) => FirebaseUserRepository()
+          create: (context) => FirebaseUserRepository(esClient: esClient)
         ),
         RepositoryProvider(
-          create: (context) => FirebaseRoomRepository()
+          create: (context) => FirebaseRoomRepository(esClient: esClient)
         )
       ],
       child: const MyApp()
